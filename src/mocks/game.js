250f05878b38a78586f872c097183bce1c467def
Vinicius <vinicius@iMac-de-Vinicius.local>
2020-06-27 19:15:04 -0300

    Finalização do App

---
+++ src/mocks/game.js
@@ -0,0 +1,21 @@
+const maps = [
+  'aquarium',
+  'breach',
+  'combine',
+  'evac',
+  'exodus',
+  'fringe',
+  'havoc',
+  'hunted',
+  'infection',
+  'metro',
+  'redwood',
+  'strongnold',
+  'nuk3town',
+  'redwood snow',
+  'frige nightfall',
+];
+
+const levels = ['recruta', 'regular', 'casca grossa', 'veterano'];
+
+export {maps, levels};
